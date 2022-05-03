a4f98eb402876a1f912ad87f184e5e3bb9c6cd75
Maurício Spinardi <mauricio.spinardi@cloudwalk.io>
2022-05-03 11:28:17 -0300

    initial commit

---
+++ app/src/main/java/io/cloudwalk/cots/poc2205031110/MainActivity.java
@@ -0,0 +1,14 @@
+package io.cloudwalk.cots.poc2205031110;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
